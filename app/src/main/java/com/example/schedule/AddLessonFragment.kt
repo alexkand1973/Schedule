8e195b62da8da29e7ea2e3e6c507b37cae66447e
alexkand1973 <Alexkand@icloud.com>
2023-05-14 18:37:57 +0300

    begining_2

---
+++ app/src/main/java/com/example/schedule/AddLessonFragment.kt
@@ -0,0 +1,14 @@
+package com.example.schedule
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AddLessonFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+    }
+}
