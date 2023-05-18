2f2eaa6221c1aae4630c77c6aa91e89d6d4ffbcf
alexkand1973 <Alexkand@icloud.com>
2023-05-18 20:52:49 +0300

    Database5

--- app/src/main/java/com/example/schedule/data/database/room/dao/LessonDao.kt
+++ app/src/main/java/com/example/schedule/data/database/room/dao/LessonDao.kt
@@ -3,34 +3,27 @@ package com.example.schedule.data.database.room.dao
 import androidx.room.*
 import com.example.schedule.data.database.room.entities.LessonDB
 
+
 @Dao
 interface LessonDao {
 
     //Помещаем урок
     @Insert
-    fun insertLesson(lesson: LessonDB) {
-
-    }
+    fun insertLesson(lesson: LessonDB)
 
     //Обновление урока
     @Update
-    fun updateLesson(lesson: LessonDB) {
-
-    }
+    fun updateLesson(lesson: LessonDB)
 
     //Удаляем урок
     @Delete
-    fun deleteLesson(lesson: LessonDB) {
-
-    }
+    fun deleteLesson(lesson: LessonDB)
 
     //Возвращаем урок
-    @Query("")
-    fun getLessonId(lessonId: Int) {
-
-    }
+    @Query("SELECT * FROM lessons_table WHERE id =:lessonId")
+    fun getLessonId(lessonId: Int)
 
     //Возвращаем расписание
-    @Query("")
+    @Query("SELECT * FROM lessons_table WHERE ")
     fun getAllLessonList(): List<LessonDB>
 }
--- app/src/main/java/com/example/schedule/data/database/room/entities/LessonDB.kt
+++ app/src/main/java/com/example/schedule/data/database/room/entities/LessonDB.kt
@@ -5,11 +5,11 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 //создаем класс для передачи данных
-@Entity
+@Entity(tableName = "lessons_table")
 data class LessonDB(
     @PrimaryKey val id: Int,
     @ColumnInfo(name = "lesson_title") val lessonTitle: String,
     @ColumnInfo(name = "lesson_description") val lessonDescription: String,
-    @ColumnInfo(name = "lesson_teacher") val lessonTeacher: String) {
-
-}
+    @ColumnInfo(name = "lesson_teacher") val lessonTeacher: String,
+    @ColumnInfo(name = "lesson_last_changed_time") val lessonLastChangedTime: String
+)
